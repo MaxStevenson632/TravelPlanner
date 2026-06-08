a16c22c25b8a3a14e5d7cea14f2694fe8b5d2518
MaxStevenson632 <max.stevenson5@outlook.com>
2026-06-08 15:28:29 +0100

    Created Waypoint Entity class
    Set up FK between WaypointEntity and TripEntity

---
+++ src/main/java/code/travelplanner/waypoint/Entity/WaypointEntity.java
@@ -0,0 +1,20 @@
+package code.travelplanner.waypoint.Entity;
+
+import code.travelplanner.trip.Entity.TripEntity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Waypoints")
+public class WaypointEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "waypoint_id")
+    private long waypointId;
+    private double latitude;
+    private double longitude;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", referencedColumnName = "tripId", nullable = false)
+    private TripEntity trip;
+}
