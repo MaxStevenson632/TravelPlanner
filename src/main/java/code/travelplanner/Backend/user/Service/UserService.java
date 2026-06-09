56f38a17e0352a83cdf017a5c2470dd433f1130f
MaxStevenson632 <max.stevenson5@outlook.com>
2026-06-09 16:46:05 +0100

    Added registerNewUser method, user's details saved to DB upon registration

---
+++ src/main/java/code/travelplanner/Backend/user/Service/UserService.java
@@ -0,0 +1,38 @@
+package code.travelplanner.Backend.user.Service;
+
+import code.travelplanner.Backend.user.Dto.UserRegisterDto;
+import code.travelplanner.Backend.user.Entity.UserEntity;
+import code.travelplanner.Backend.user.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void registerNewUser(UserRegisterDto userRegistrationData) {
+        System.out.println("Method using");
+        // New row in DB
+        UserEntity user = new UserEntity();
+        user.setName(userRegistrationData.getUsername());
+
+        // Encrypt the password using Bcrypt hashing
+        user.setPassword(passwordEncoder.encode(userRegistrationData.getPassword()));
+
+        user.setEmail(userRegistrationData.getEmail());
+        userRepository.save(user);
+        System.out.println("Entered");
+    }
+
+
+
+}
