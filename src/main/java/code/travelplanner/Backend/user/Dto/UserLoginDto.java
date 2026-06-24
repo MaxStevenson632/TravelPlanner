3b91a6577ecd9f918f363117630e707042ef90c1
MaxStevenson632 <max.stevenson5@outlook.com>
2026-06-24 10:36:29 +0100

    Created UserLoginDto.java

---
+++ src/main/java/code/travelplanner/Backend/user/Dto/UserLoginDto.java
@@ -0,0 +1,14 @@
+package code.travelplanner.Backend.user.Dto;
+
+public class UserLoginDto {
+
+    private String username;
+    private String password;
+
+    public String getUsername() { return username; }
+    public void setUsername(String newUsername) { username = newUsername; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String newPassword) { password = newPassword; }
+
+}
