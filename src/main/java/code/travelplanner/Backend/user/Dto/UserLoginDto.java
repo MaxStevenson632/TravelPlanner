23f919ec92d21a0ef0749fb649d147dcba6f82e2
MaxStevenson632 <max.stevenson5@outlook.com>
2026-06-25 13:32:38 +0100

    Changed username field to email field
    Users should log in via email, names can be identical

--- src/main/java/code/travelplanner/Backend/user/Dto/UserLoginDto.java
+++ src/main/java/code/travelplanner/Backend/user/Dto/UserLoginDto.java
@@ -2,11 +2,11 @@ package code.travelplanner.Backend.user.Dto;
 
 public class UserLoginDto {
 
-    private String username;
+    private String email;
     private String password;
 
-    public String getUsername() { return username; }
-    public void setUsername(String newUsername) { username = newUsername; }
+    public String getEmail() { return email; }
+    public void setEmail(String newEmail) { email = newEmail; }
 
     public String getPassword() { return password; }
     public void setPassword(String newPassword) { password = newPassword; }
