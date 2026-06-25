ad5f5ce773a26ab1b9332ceecb90a4c41de504a3
MaxStevenson632 <max.stevenson5@outlook.com>
2026-06-25 13:40:59 +0100

    Added redirection to /home page after login and error handling

--- src/main/java/code/travelplanner/Frontend/script/login.js
+++ src/main/java/code/travelplanner/Frontend/script/login.js
@@ -1,18 +1,35 @@
 async function loginToAccount() {
-    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    const response = await fetch("http://localhost:8080/travelplanner/login", {
-        method: "POST",
-        body: JSON.stringify({
-            username: username,
-            password: password,
-        }),
-        credentials: 'include',
-        headers: {
-            "content-type": "application/json; charset=UTF-8"
+    try {
+
+        // Wait for response, a raw data stream
+        const response = await fetch("http://localhost:8080/travelplanner/login", {
+            method: "POST",
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            }),
+            credentials: 'include',
+            headers: {
+                "content-type": "application/json; charset=UTF-8"
+            }
+        })
+
+        if (response.ok) {
+
+            // Parse raw data into JSON object
+            const data = await response.json();
+            console.log("Login successful");
+            // Redirect to /home page after successful login
+            window.location.href = "./home.html";
+
+        } else {
+            alert("Invalid email or password");
         }
-    })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+
+    } catch (error) {
+        console.log("Error logging into account", error);
+    }
 }
