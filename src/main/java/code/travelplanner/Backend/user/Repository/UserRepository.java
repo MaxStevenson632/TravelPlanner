32b85fad76d67b0c0c4d8cab7cbacc997c816627
MaxStevenson632 <max.stevenson5@outlook.com>
2026-06-25 13:37:45 +0100

    findByEmail changed to return Optional, removed findByUsername
    Handles null values better

--- src/main/java/code/travelplanner/Backend/user/Repository/UserRepository.java
+++ src/main/java/code/travelplanner/Backend/user/Repository/UserRepository.java
@@ -3,9 +3,9 @@ package code.travelplanner.Backend.user.Repository;
 import code.travelplanner.Backend.user.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long>  {
 
-    UserEntity findByEmail(String email);
-
-    UserEntity findByUsername(String username);
+    Optional<UserEntity> findByEmail(String email);
 }
