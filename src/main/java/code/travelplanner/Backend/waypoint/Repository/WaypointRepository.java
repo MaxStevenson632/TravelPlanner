9afe7d90c94912083cd27258358109276fd4413c
MaxStevenson632 <max.stevenson5@outlook.com>
2026-07-15 16:11:06 +0100

    Added findByPlaceName method

--- src/main/java/code/travelplanner/Backend/waypoint/Repository/WaypointRepository.java
+++ src/main/java/code/travelplanner/Backend/waypoint/Repository/WaypointRepository.java
@@ -3,6 +3,9 @@ package code.travelplanner.Backend.waypoint.Repository;
 import code.travelplanner.Backend.waypoint.Entity.WaypointEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface WaypointRepository extends JpaRepository<WaypointEntity, Long>  {
 
+    Optional<WaypointEntity> findByPlaceName(String placeName);
 }
