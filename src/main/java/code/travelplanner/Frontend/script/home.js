e2940ea03c9d8514b8b9e597b883233e6074b5e1
MaxStevenson632 <max.stevenson5@outlook.com>
2026-07-15 16:46:01 +0100

    Created home.js, added eventListeners for the addTrip, cancelTrip buttons and tripForm submit button

---
+++ src/main/java/code/travelplanner/Frontend/script/home.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const AddTripBtn = document.getElementById("addTripBtn");
+    const cancelTripBtn = document.getElementById("cancelTripBtn");
+    const tripFormContainer = document.getElementById("tripFormContainer");
+    const tripForm = document.getElementById("tripForm");
+
+    AddTripBtn.addEventListener("click", () => {
+        tripFormContainer.classList.remove("hidden");
+    });
+
+    cancelTripBtn.addEventListener("click", () => {
+        tripFormContainer.classList.add("hidden");
+        tripForm.reset();
+    });
+
+    tripForm.addEventListener("submit", async (e) => {
+
+        e.preventDefault();
+
+        const waypointsArray = document.getElementById("waypoints").value
+            // Separate all values with commas
+            .split(",")
+            // Create new array, remove whitespaces on each end of each item
+            .map(item => item.trim())
+            // No empty strings
+            .filter(item => item !== "");
+
+        const title = document.getElementById("title").value;
+        const waypoints = waypointsArray;
+        const startDate = document.getElementById("startDate").value;
+        const endDate = document.getElementById("endDate").value;
+
+        try {
+            const response = await fetch("http://localhost:8080/travelplanner/createTrip",  {
+                method: "POST",
+                body: JSON.stringify({
+                    title: title,
+                    waypoints: waypoints,
+                    startDate: startDate,
+                    endDate: endDate,
+                }),
+                credentials: 'include',
+                headers: {
+                    "content-type": "application/json; charset=UTF-8"
+                }
+            })
+
+
+        } catch (e) {
+
+        }
+
+
+    })
+})
