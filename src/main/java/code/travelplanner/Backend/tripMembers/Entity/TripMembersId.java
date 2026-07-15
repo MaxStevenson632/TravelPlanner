84fcbaaa55289eda86aafbe4db8fb6b4f2dc83ed
MaxStevenson632 <max.stevenson5@outlook.com>
2026-07-15 16:26:19 +0100

    Added equals and hashCode methods

--- src/main/java/code/travelplanner/Backend/tripMembers/Entity/TripMembersId.java
+++ src/main/java/code/travelplanner/Backend/tripMembers/Entity/TripMembersId.java
@@ -1,13 +1,18 @@
-package code.travelplanner.Backend.trip.Entity;
+package code.travelplanner.Backend.tripMembers.Entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class TripMembersId implements Serializable {
 
+    @Column(name = "trip_id")
     private long tripId;
+
+    @Column(name = "user_id")
     private long userId;
 
     public TripMembersId() {}
@@ -23,4 +28,17 @@ public class TripMembersId implements Serializable {
     public long getUserId() { return this.userId; }
     public void setUserId(long userId) { this.userId = userId; }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TripMembersId)) return false;
+        TripMembersId that = (TripMembersId) o;
+        return Objects.equals(userId, that.userId) &&
+                Objects.equals(tripId, that.tripId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, tripId);
+    }
 }
