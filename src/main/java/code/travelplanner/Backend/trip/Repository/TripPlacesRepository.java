d95b4023e7e856667e97206b32b91e8b5bef1c37
MaxStevenson632 <max.stevenson5@outlook.com>
2026-08-07 18:55:47 +0100

    Added findByTripIdOrderByVisitOrder query

--- src/main/java/code/travelplanner/Backend/trip/Repository/TripPlacesRepository.java
+++ src/main/java/code/travelplanner/Backend/trip/Repository/TripPlacesRepository.java
@@ -4,7 +4,9 @@ import code.travelplanner.Backend.trip.Entity.TripEntity;
 import code.travelplanner.Backend.trip.Entity.TripPlacesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TripPlacesRepository extends JpaRepository<TripPlacesEntity, Long> {
 
-
+    List<TripPlacesEntity> findByTripIdOrderByVisitOrder(long tripId);
 }
