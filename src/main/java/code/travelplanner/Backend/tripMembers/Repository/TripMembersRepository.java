99cfa969a5a9095e275ce34e9ee660f253b9e108
MaxStevenson632 <max.stevenson5@outlook.com>
2026-08-07 20:00:11 +0100

    Added findByIdTripIdAndIdUserId and findByUserId queries

--- src/main/java/code/travelplanner/Backend/tripMembers/Repository/TripMembersRepository.java
+++ src/main/java/code/travelplanner/Backend/tripMembers/Repository/TripMembersRepository.java
@@ -4,6 +4,12 @@ import code.travelplanner.Backend.trip.Entity.TripEntity;
 import code.travelplanner.Backend.tripMembers.Entity.TripMembersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TripMembersRepository extends JpaRepository<TripMembersEntity, Long> {
 
+    Optional<TripMembersEntity> findByIdTripIdAndIdUserId(long  tripId, long userId);
+
+    List<TripMembersEntity> findByIdUserId(long userId);
 }
