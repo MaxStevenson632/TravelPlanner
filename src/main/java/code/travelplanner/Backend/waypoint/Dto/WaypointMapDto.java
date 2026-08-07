6d06b9b3318ef98c9f7d77c82b2c5cfca513d680
MaxStevenson632 <max.stevenson5@outlook.com>
2026-08-07 20:01:17 +0100

    Created WaypointMapDto.java

---
+++ src/main/java/code/travelplanner/Backend/waypoint/Dto/WaypointMapDto.java
@@ -0,0 +1,21 @@
+package code.travelplanner.Backend.waypoint.Dto;
+
+public class WaypointMapDto {
+
+    private String placeName;
+    private double latitude;
+    private double longitude;
+    private int visitOrder;
+
+    public String getPlaceName() { return placeName; }
+    public void setPlaceName(String placeName) { this.placeName = placeName; }
+
+    public double getLatitude() { return latitude; }
+    public void setLatitude(double latitude) { this.latitude = latitude; }
+
+    public double getLongitude() { return longitude; }
+    public void setLongitude(double longitude) { this.longitude = longitude; }
+
+    public int getVisitOrder() { return visitOrder; }
+    public void setVisitOrder(int visitOrder) { this.visitOrder = visitOrder; }
+}
