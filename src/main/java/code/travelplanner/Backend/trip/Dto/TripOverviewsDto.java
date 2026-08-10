9d602475945a1743c3d3efb70dff9b87673df0b4
MaxStevenson632 <max.stevenson5@outlook.com>
2026-08-10 15:03:32 +0100

    Added id field
    Represents tripId for the frontend to use

--- src/main/java/code/travelplanner/Backend/trip/Dto/TripOverviewsDto.java
+++ src/main/java/code/travelplanner/Backend/trip/Dto/TripOverviewsDto.java
@@ -6,10 +6,14 @@ public class TripOverviewsDto {
 
     private String name;
     private Role role;
+    private long id;
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
     public Role getRole() { return role; }
     public void setRole(Role role) { this.role = role; }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 }
