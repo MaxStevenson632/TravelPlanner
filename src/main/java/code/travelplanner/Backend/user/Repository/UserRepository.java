3007f1c7111cf352c110c328a2afb330f5c0e9f1
MaxStevenson632 <max.stevenson5@outlook.com>
2026-08-14 16:27:06 +0100

    Added searchUsersExludingSelfAndOtherMembers query

--- src/main/java/code/travelplanner/Backend/user/Repository/UserRepository.java
+++ src/main/java/code/travelplanner/Backend/user/Repository/UserRepository.java
@@ -2,12 +2,28 @@ package code.travelplanner.Backend.user.Repository;
 
 import code.travelplanner.Backend.user.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long>  {
 
     Optional<UserEntity> findByEmail(String email);
 
+    Optional<UserEntity> findByName(String name);
+
     Optional<UserEntity> findByUserId(Long userId);
+
+    @Query("""
+            SELECT user from UserEntity user
+            WHERE ((user.name LIKE CONCAT('%', :query, '%'))
+            OR user.email LIKE CONCAT('%', :query, '%'))
+            AND user.userId <> :requesterId
+                        AND user.userId NOT IN (
+                        SELECT tripMember.id.userId FROM TripMembersEntity tripMember WHERE tripMember.id.tripId = :tripId)
+            """)
+    List<UserEntity> searchUsersExcludingSelfAndOtherMembers(@Param("query")  String query, @Param("tripId") Long tripId,
+                                                             @Param("requesterId") Long requesterId);
 }
