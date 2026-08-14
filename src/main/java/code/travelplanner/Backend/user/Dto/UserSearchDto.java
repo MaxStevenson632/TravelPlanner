edc639d823dd35784415e2951e7e35d58fb670a2
MaxStevenson632 <max.stevenson5@outlook.com>
2026-08-14 16:25:52 +0100

    Created UserSearchDto

---
+++ src/main/java/code/travelplanner/Backend/user/Dto/UserSearchDto.java
@@ -0,0 +1,23 @@
+package code.travelplanner.Backend.user.Dto;
+
+public class UserSearchDto {
+
+    private Long userId;
+    private String name;
+    private String email;
+
+    public UserSearchDto(Long userId, String name, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
