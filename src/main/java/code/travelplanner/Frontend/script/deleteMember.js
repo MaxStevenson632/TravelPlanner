0ce68ad52d1f4a204e04a058d45e3d5a5f92c893
MaxStevenson632 <max.stevenson5@outlook.com>
2026-08-25 13:46:19 +0100

    Created deleteMember.js, added deleteMember, deleteMode and exitDeleteMode functions

---
+++ src/main/java/code/travelplanner/Frontend/script/deleteMember.js
@@ -0,0 +1,47 @@
+import { token } from "./auth.js";
+
+export async function deleteMember(userId, tripId) {
+
+    console.log("In function");
+    try {
+        const response = await fetch(`http://localhost:8080/travelplanner/${tripId}/members/deleteMember/${userId}`,
+            {
+                method: 'DELETE',
+                headers: {
+                    "Authorization" : `Bearer ${token}`,
+                    "Content-Type" : "application/json"
+                }
+            }
+        );
+
+        if (!response.ok) {
+            alert("Issue removing member from trip");
+        }
+
+    } catch (error) {
+        console.log("Error deleting member from trip", error);
+    }
+}
+
+export function deleteMode() {
+
+    const deleteButton = document.getElementById("deleteMemberBtn");
+    deleteButton.classList.add("is-active");
+
+    // Highlight all rows where person is not OWNER
+    let nonOwnerMembers = document.querySelectorAll(".person-row:not([data-user-role='OWNER'])");
+    nonOwnerMembers.forEach(row => {
+        row.classList.add("delete-mode");
+    })
+}
+
+export function exitDeleteMode() {
+
+    const deleteButton = document.getElementById("deleteMemberBtn");
+    deleteButton.classList.remove("is-active");
+
+    let highlightedRows = document.querySelectorAll(".person-row.delete-mode");
+    highlightedRows.forEach(row => {
+        row.classList.remove("delete-mode");
+    });
+}
