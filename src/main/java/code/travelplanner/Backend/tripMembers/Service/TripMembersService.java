28b342d6bc829d95be3780c5543298886865a26b
MaxStevenson632 <max.stevenson5@outlook.com>
2026-08-25 13:50:48 +0100

    Added deletePersonFromTrip and editPersonRole methods

--- src/main/java/code/travelplanner/Backend/tripMembers/Service/TripMembersService.java
+++ src/main/java/code/travelplanner/Backend/tripMembers/Service/TripMembersService.java
@@ -56,4 +56,48 @@ public class TripMembersService {
         member.setMemberRole(userRole);
         tripMembersRepository.save(member);
     }
+
+    public void deletePersonFromTrip(Long requesterId, Long memberToDeleteId, Long tripId) {
+
+        // Verify user belongs to trip
+        TripMembersEntity memberCheck = tripMembersRepository.findByIdTripIdAndIdUserId(tripId, requesterId)
+                .orElseThrow(() -> new IllegalArgumentException("Error matching user with trip"));
+
+        // Verify user has permission to delete a member
+        if (!(memberCheck.getMemberRole().equals(Role.OWNER))) {
+            throw new  IllegalArgumentException("User does not have permission to add member");
+        }
+
+        tripMembersRepository.deleteByIdUserId(memberToDeleteId, tripId);
+    }
+
+    public void editPersonRole(Long tripId, Long requesterId, String newRole, Long personToEditId)  {
+
+        // Verify user belongs to trip
+        TripMembersEntity memberCheck = tripMembersRepository.findByIdTripIdAndIdUserId(tripId, requesterId)
+                .orElseThrow(() -> new IllegalArgumentException("Error matching user with trip"));
+
+        // Verify user has permission to edit a member's role
+        if (!(memberCheck.getMemberRole().equals(Role.OWNER))) {
+            throw new  IllegalArgumentException("User does not have permission to add member");
+        }
+
+        TripMembersEntity personToEdit = tripMembersRepository.findByIdTripIdAndIdUserId(tripId, personToEditId)
+                .orElseThrow(() -> new  IllegalArgumentException("Error matching user with trip"));
+
+        // Remove quotation marks from string
+        String cleanRole = newRole.replace("\"", "").trim();
+        Role castedRole = null;
+
+        if (cleanRole.equals("VIEWER")) {
+            castedRole = Role.VIEWER;
+        } else if (cleanRole.equals("MEMBER")) {
+            castedRole = Role.MEMBER;
+        } else {
+            throw new IllegalArgumentException("Invalid role");
+        }
+
+        personToEdit.setMemberRole(castedRole);
+        tripMembersRepository.save(personToEdit);
+    }
 }
