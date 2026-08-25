7cd63b9a32e89c51b18ad54045de3910dec7502c
MaxStevenson632 <max.stevenson5@outlook.com>
2026-08-25 13:46:41 +0100

    Created editMember.js, added editTripMember function

---
+++ src/main/java/code/travelplanner/Frontend/script/editMember.js
@@ -0,0 +1,23 @@
+import {token} from "./auth.js";
+
+export async function editTripMember(userId, tripId, newRole) {
+
+    try {
+
+        const response = await fetch(`http://localhost:8080/travelplanner/${tripId}/members/editMemberRole/${userId}`, {
+            method: 'PUT',
+            headers: {
+                "Authorization" : `Bearer ${token}`,
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(newRole)
+        });
+
+        if (!response.ok) {
+            alert("Error switching member's role");
+        }
+
+    } catch (error) {
+        console.log("Error editing member's role", error);
+    }
+}
