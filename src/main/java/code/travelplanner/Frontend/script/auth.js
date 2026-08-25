b644fab060e22f2a3be48f729164928145e2a07f
MaxStevenson632 <max.stevenson5@outlook.com>
2026-08-25 13:42:48 +0100

    Added getUserIdFromToken function

--- src/main/java/code/travelplanner/Frontend/script/auth.js
+++ src/main/java/code/travelplanner/Frontend/script/auth.js
@@ -25,3 +25,29 @@ export function getAccountNameFromToken() {
         return null;
     }
 }
+
+export function getUserIdFromToken() {
+
+    try {
+
+        // Split the token apart, use the payload (second part)
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+
+        // Decode JSON string
+        const jsonPayload = decodeURIComponent(
+            atob(base64)
+                .split('')
+                .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+                .join('')
+        );
+
+        const payload = JSON.parse(jsonPayload);
+
+        return Number(payload.sub);
+
+    } catch (error) {
+        console.error("Failed to decode JWT:", error);
+        return null;
+    }
+}
