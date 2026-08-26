040e3a20f38c8e0abcda82d9f1c43d89f89e50af
MaxStevenson632 <max.stevenson5@outlook.com>
2026-08-26 14:54:20 +0100

    Created TripPlaceController.java, added addWaypointToTrip method

---
+++ src/main/java/code/travelplanner/Backend/trip/Controller/TripPlacesController.java
@@ -0,0 +1,30 @@
+package code.travelplanner.Backend.trip.Controller;
+
+import code.travelplanner.Backend.trip.Service.TripPlacesService;
+import code.travelplanner.Backend.waypoint.Dto.WaypointMapDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RequestMapping("/travelplanner/{tripId}")
+@Controller
+@CrossOrigin(origins = "http://localhost:63342", allowCredentials = "true")
+public class TripPlacesController {
+
+    private final TripPlacesService tripPlacesService;
+
+    public TripPlacesController(TripPlacesService tripPlacesService) {
+        this.tripPlacesService = tripPlacesService;
+    }
+
+    @PostMapping("/addWaypoint")
+    public ResponseEntity<?> addWaypointToTrip(@RequestBody WaypointMapDto body,
+                                               @PathVariable Long tripId, @AuthenticationPrincipal Long requesterId) {
+
+        tripPlacesService.addWaypointToTrip(tripId, body, requesterId);
+        return ResponseEntity.ok().build();
+    }
+}
