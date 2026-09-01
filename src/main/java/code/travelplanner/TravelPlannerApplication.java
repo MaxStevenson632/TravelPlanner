06a24ba1b8ca68dcb9400383bfbf272d77eba6a8
MaxStevenson632 <max.stevenson5@outlook.com>
2026-09-01 14:04:44 +0100

    Added @EnableCaching annotation

--- src/main/java/code/travelplanner/TravelPlannerApplication.java
+++ src/main/java/code/travelplanner/TravelPlannerApplication.java
@@ -2,12 +2,13 @@ package code.travelplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TravelPlannerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TravelPlannerApplication.class, args);
 	}
-
 }
