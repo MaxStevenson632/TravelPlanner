0a207cecbe9f903b646bf574f86ffaebc442c00d
MaxStevenson632 <max.stevenson5@outlook.com>
2026-09-01 20:17:27 +0100

    Created WaypointController.java, added getTopFourPlaces method with /waypoint/search endpoint

---
+++ src/main/java/code/travelplanner/Backend/waypoint/Controller/WaypointController.java
@@ -0,0 +1,30 @@
+package code.travelplanner.Backend.waypoint.Controller;
+
+import code.travelplanner.Backend.waypoint.Dto.GeocodingResponse;
+import code.travelplanner.Backend.waypoint.Service.GeocodingService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping("/travelplanner")
+@RestController
+@CrossOrigin(origins = "http://localhost:63342", allowCredentials = "true")
+public class WaypointController {
+
+    private final GeocodingService geocodingService;
+
+    public  WaypointController(GeocodingService geocodingService) {
+        this.geocodingService = geocodingService;
+    }
+
+    @GetMapping("/waypoint/search")
+    public List<GeocodingResponse> GetTopFourPlaces(@RequestParam String query) {
+
+        List<GeocodingResponse> places = geocodingService.searchFourPlaces(query);
+        return places;
+    }
+
+}
