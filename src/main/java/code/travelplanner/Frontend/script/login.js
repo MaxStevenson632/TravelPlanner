496191a719a0916046860f7b42e8eaa1db5c0343
MaxStevenson632 <max.stevenson5@outlook.com>
2026-09-02 14:12:09 +0100

    email and password now fetch from their new id names

--- src/main/java/code/travelplanner/Frontend/script/login.js
+++ src/main/java/code/travelplanner/Frontend/script/login.js
@@ -1,6 +1,6 @@
 async function loginToAccount() {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+    const email = document.getElementById("emailEntry").value;
+    const password = document.getElementById("passwordEntry").value;
 
     try {
 
