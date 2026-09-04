beaac7c0a93de6ea605b552b3f6b1376f20b305e
MaxStevenson632 <max.stevenson5@outlook.com>
2026-09-04 17:09:54 +0100

    Injected mailSender to constructor

--- src/main/java/code/travelplanner/Backend/user/Service/EmailService.java
+++ src/main/java/code/travelplanner/Backend/user/Service/EmailService.java
@@ -4,6 +4,7 @@ import code.travelplanner.Backend.configuration.EmailConfiguration;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -14,19 +15,15 @@ public class EmailService {
 
     @Autowired
     private EmailConfiguration emailConfiguration;
+    private final JavaMailSender mailSender;
 
-    public JavaMailSender getMailSender() {
-        JavaMailSenderImpl mailSenderImpl = new JavaMailSenderImpl();
-        mailSenderImpl.setHost(emailConfiguration.getHost());
-        mailSenderImpl.setPort(emailConfiguration.getPort());
-        mailSenderImpl.setUsername(emailConfiguration.getUsername());
-        mailSenderImpl.setPassword(emailConfiguration.getPassword());
-        return mailSenderImpl;
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
     }
 
     public void sendVerificationEmail(String toEmail, String token) throws MessagingException {
 
-        MimeMessage mimeMessage = getMailSender().createMimeMessage();
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
 
         // Enable HTML elements
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
@@ -34,7 +31,7 @@ public class EmailService {
         // Email address and subject
         helper.setTo(toEmail);
         helper.setSubject("Verify your TravelPlanner account");
-        helper.setFrom("no-reply@travelplanner.com");
+        helper.setFrom("wayvelapp@gmail.com");
 
         // Link to backend endpoint
         String verificationUrl = "http://localhost:8080/travelplanner/verify?token=" + token;
@@ -47,6 +44,6 @@ public class EmailService {
         // Spring Boot renders this text as html
         helper.setText(htmlContent, true);
 
-        getMailSender().send(mimeMessage);
+        mailSender.send(mimeMessage);
     }
 }
