332c6e1fba6356cf5b1cb796646badf78a096c7e
MaxStevenson632 <max.stevenson5@outlook.com>
2026-09-09 17:50:05 +0100

    Initial zoom set to 1.5 and enable scrollZoom

--- src/main/java/code/travelplanner/Frontend/script/Map/mapInit.js
+++ src/main/java/code/travelplanner/Frontend/script/Map/mapInit.js
@@ -15,12 +15,11 @@ export async function createMap() {
         container: 'map',
         style: 'mapbox://styles/mapbox/standard', // Use the standard style for the map
         projection: 'equirectangular', // display the map as a rectangle
-        zoom: 1, // initial zoom level, 0 is the world view, higher values zoom in
+        zoom: 1.5, // initial zoom level, 0 is the world view, higher values zoom in
         center: [30, 15] // center the map on this longitude and latitude
     });
 
     map.addControl(new mapboxgl.NavigationControl());
-    map.scrollZoom.disable();
 
     map.on('style.load', () => {
         map.setFog({}); // Set the default atmosphere style
