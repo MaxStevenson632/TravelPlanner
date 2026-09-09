64d3d2bcd4aaa883e13e990ea81c0041d9c0f26d
MaxStevenson632 <max.stevenson5@outlook.com>
2026-09-09 14:50:32 +0100

    Changed sendVerificationEmail to send http post request to brevo instead of SMTP

--- src/main/java/code/travelplanner/Backend/user/Service/EmailService.java
+++ src/main/java/code/travelplanner/Backend/user/Service/EmailService.java
@@ -4,52 +4,65 @@ import code.travelplanner.Backend.configuration.EmailConfiguration;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class EmailService {
 
+    @Value("${brevo.api-key}")
+    private String brevoApiKey;
+
     @Autowired
     private EmailConfiguration emailConfiguration;
-    private final JavaMailSender mailSender;
+    private final RestClient brevoRestClient;
 
-    public EmailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
+    public EmailService(JavaMailSender mailSender, RestClient brevoRestClient) {
+        this.brevoRestClient = brevoRestClient;
     }
 
     public void sendVerificationEmail(String toEmail, String token) throws MessagingException {
 
-        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        String verificationUrl = "https://travelplanner-backend-vd34.onrender.com/travelplanner/verify?token=" + token;
 
-        // Enable HTML elements
-        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+        String htmlContent =
+                "<h3>Welcome to Travel Planner!</h3>"
+                        + "<p>Please click the link below to verify your account:</p>"
+                        + "<p><a href='" + verificationUrl + "'>Verify Account</a></p>";
 
-        // Email address and subject
-        helper.setTo(toEmail);
-        helper.setSubject("Verify your TravelPlanner account");
-        helper.setFrom("wayvelapp@gmail.com");
+        Map<String, Object> sender = Map.of(
+                "name", "TravelPlanner",
+                "email", "wayvelapp@gmail.com"
+        );
 
-        // Link to backend endpoint
-        String verificationUrl = "http://localhost:8080/travelplanner/verify?token=" + token;
+        Map<String, Object> recipient = Map.of(
+                "email", toEmail
+        );
 
-        // HTML code for a clickable hyperlink
-        String htmlContent = "<h3>Welcome to Travel Planner!</h3>"
-                + "<p>Please click the link below to verify your account:</p>"
-                + "<a href='" + verificationUrl + "'>Verify Account</a>";
+        Map<String, Object> requestBody = Map.of(
+                "sender", sender,
+                "to", List.of(recipient),
+                "subject", "Verify your TravelPlanner account",
+                "htmlContent", htmlContent
+        );
 
-        // Spring Boot renders this text as html
-        helper.setText(htmlContent, true);
+        // HTTP POST request to https://api.brevo.com/v3/smtp/email
+        // Content is JSON
+        brevoRestClient.post()
+                .uri("/v3/smtp/email")
+                .header("api-key", brevoApiKey)
+                .header("Content-Type", "application/json")
+                .body(requestBody)
+                .retrieve()
+                .toBodilessEntity();
 
-        try {
-            mailSender.send(mimeMessage);
-            System.out.println("EMAIL SENT SUCCESSFULLY TO: " + toEmail);
-        } catch (Exception e) {
-            System.err.println("EMAIL SENDING FAILED");
-            e.printStackTrace();
-        }
     }
 }
