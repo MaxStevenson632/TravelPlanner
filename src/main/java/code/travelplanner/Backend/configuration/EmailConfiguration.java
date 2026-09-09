42a80870ab54ca6764aecb0534795dc57c1d63dc
MaxStevenson632 <max.stevenson5@outlook.com>
2026-09-09 14:49:15 +0100

    Added brevoRestClient Bean and removed getJavaMailSender Bean
    Emails sent over https, not smtp

--- src/main/java/code/travelplanner/Backend/configuration/EmailConfiguration.java
+++ src/main/java/code/travelplanner/Backend/configuration/EmailConfiguration.java
@@ -5,38 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 
 import java.util.Properties;
 
 @Component
 public class EmailConfiguration {
 
-    @Value("${spring.mail.host}")
-    private String host;
-    @Value("${spring.mail.port}")
-    private int port;
-    @Value("${spring.mail.username}")
-    private String username;
-    @Value("${spring.mail.password}")
-    private String password;
-
     @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-
-        mailSender.setHost(host);
-        mailSender.setPort(port);
-        mailSender.setUsername(username);
-        mailSender.setPassword(password);
-
-        // Use the properties for mail smtp
-        Properties props = mailSender.getJavaMailProperties();
-
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.starttls.required", "true");
-        props.put("mail.smtp.sll.enable", "false");
-
-        return mailSender;
+    public RestClient brevoRestClient() {
+        // Brevo url
+        return RestClient.builder()
+                .baseUrl("https://api.brevo.com")
+                .build();
     }
 }
